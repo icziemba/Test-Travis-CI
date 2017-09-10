443132023dcfb1194c20ae05e53bc7c4b20b574d
Ian Ziemba <icziemba@gmail.com>
2017-09-09 20:41:31 -0500

    Updated project for gradle testing.

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+	public String getGreeting() {
+		return "Hello world.";
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(new HelloWorld().getGreeting());
+	}
+
+}
